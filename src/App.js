255f6e1e192629348f2667f1a4c0dc14c5ad3ff5
Oleh Kutsenko <inthedark1225@gmail.com>
2021-01-30 01:52:03 +0200

    step-5 Grid component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import { useState } from 'react';
+import {Grid} from './Grid/Grid';
 
 export function App() {
- const [count, setCount] = useState(0);
- const handleUpClick = (event) => {
-   setCount((prevCount) => prevCount - 1)
- }
- const handleDownClick = () => {
-   setCount((prevCount) => prevCount + 1)
- }
-
- return (
-   <div>
-     <button onClick={handleUpClick}>-</button>
-     <button onClick={handleDownClick}>+</button>
-     <span> : </span>
-     <span>{count}</span>
-   </div>
- );
+  return (
+    <div>
+      <Grid></Grid>
+    </div>
+  );
 }
---
+++ src/Grid/Grid.js
@@ -0,0 +1,8 @@
+// Grid.js
+import styles from './Grid.module.css'
+
+
+export function Grid() {
+   return "Grid"
+}
+
