6912cadea808a759ba5996419aadb309db62bb2d
Oleh Kutsenko <inthedark1225@gmail.com>
2021-01-30 01:19:46 +0200

    init study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
-function App() {
+export function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className={styles.app}>
+      <div className={styles.logo}>
+          <img src={logo} alt="logo" />
+      </div>
+      <a href="#">
+        Переключить логотип
+      </a>
+    </header>
   );
 }
-
-export default App;
