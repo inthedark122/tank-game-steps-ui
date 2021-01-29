2f9396265c8b56e547c45a1d8e73fd86fac03372
Oleh Kutsenko <inthedark1225@gmail.com>
2021-01-30 01:41:47 +0200

    step-4 buttons with counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 
 export function App() {
-  const count = 0;
+ const [count, setCount] = useState(0);
+ const handleUpClick = (event) => {
+   setCount((prevCount) => prevCount - 1)
+ }
+ const handleDownClick = () => {
+   setCount((prevCount) => prevCount + 1)
+ }
 
-  return (
-    <div>
-      <button>-</button>
-      <button>+</button>
-      <span> : </span>
-      <span>{count}</span>
-    </div>
-  );
+ return (
+   <div>
+     <button onClick={handleUpClick}>-</button>
+     <button onClick={handleDownClick}>+</button>
+     <span> : </span>
+     <span>{count}</span>
+   </div>
+ );
 }
