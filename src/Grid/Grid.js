bfa81c6385a71c0ef39608c638392de9b1a67be6
Oleh Kutsenko <inthedark1225@gmail.com>
2021-01-30 01:55:03 +0200

    step-6 grid for tanks

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,8 +1,12 @@
-// Grid.js
 import styles from './Grid.module.css'
-
+const GRID_SIZE = 20
 
 export function Grid() {
-   return "Grid"
+   return Array.from({ length: GRID_SIZE }).map((_, y) => (
+       <div className={styles.row}>
+           {Array.from({ length: GRID_SIZE }).map((_, x) => (
+               <span className={styles.cell}></span>
+           ))}
+       </div>
+   ))
 }
-
