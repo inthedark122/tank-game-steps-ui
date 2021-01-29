259496daa5c4d4fbafd44aa9dbf94c6c133514f3
Oleh Kutsenko <inthedark1225@gmail.com>
2021-01-30 01:38:41 +0200

    step-3 buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import { useState } from 'react';
-import logo from './logo.svg';
-import styles from './App.module.css';
 
 export function App() {
-  const [showLogo, setShowLogo] = useState(true);
-  const handleClick = (event) => {
-    event.preventDefault()
-    setShowLogo((prevShowLogo) => !prevShowLogo)
-  }
+  const count = 0;
 
   return (
-    <header className={styles.app}>
-      <div className={styles.logo}>
-        {showLogo && <img src={logo} alt="logo" />}
-      </div>
-      <a onClick={handleClick} href="#">
-        Переключить логотип
-      </a>
-    </header>
+    <div>
+      <button>-</button>
+      <button>+</button>
+      <span> : </span>
+      <span>{count}</span>
+    </div>
   );
 }
