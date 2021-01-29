04b8b070c7b7d5fbe77a9110ce483bca9f538a8c
Oleh Kutsenko <inthedark1225@gmail.com>
2021-01-30 01:22:14 +0200

    click handler

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 
 export function App() {
+  const handleClick = (event) => {
+    alert("Вы нажали на ссылку")
+  }
 
   return (
     <header className={styles.app}>
       <div className={styles.logo}>
-          <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" />
       </div>
-      <a href="#">
+      <a onClick={handleClick} href="#">
         Переключить логотип
       </a>
     </header>
