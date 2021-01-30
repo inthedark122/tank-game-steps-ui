b3ea437e96b3ecf2e7de268ffa68f6a74240ea76
Oleh Kutsenko <inthedark1225@gmail.com>
2021-01-30 02:18:55 +0200

    step-11 tank position

--- src/Tank/Tank.jsx
+++ src/Tank/Tank.jsx
@@ -1,6 +1,24 @@
+import {useState} from 'react';
 import { ReactComponent as TankIcon } from '../icons/tank.svg'
 import styles from './Tank.module.css'
 
+const rotates = {
+    "top": 0,
+    "right": 90,
+    "bottom": 180,
+    "left": 270
+}
+
 export function Tank() {
-    return <TankIcon className={styles.tank} />
+    const [position, setPosition] = useState({ x: 5, y: 0 })
+    const [rotate, setRotate] = useState("right")
+
+    return <TankIcon
+        className={styles.tank}
+        style={{
+            top: position.y * 24,
+            left: position.x * 24,
+            transform: `rotate(${rotates[rotate]}deg)`
+        }}
+    />
 }
