543bccd325d53925cef005d7f5715b44d5c839fb
Oleh Kutsenko <inthedark1225@gmail.com>
2021-01-30 02:01:16 +0200

    step-8 tank icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './ws/ws'
 import {Grid} from './Grid/Grid';
+import { ReactComponent as Tank } from './icons/tank.svg'
 
 export function App() {
   return (
     <div>
       <Grid></Grid>
+      <Tank />
     </div>
   );
 }
