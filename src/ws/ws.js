325cbc7e4a687521382ef9e6e5d7212862ffef36
Oleh Kutsenko <inthedark1225@gmail.com>
2021-01-30 02:10:21 +0200

    step-10 init tank

--- src/ws/ws.js
+++ src/ws/ws.js
@@ -2,7 +2,11 @@ export const ws = new WebSocket("ws://localhost:8080")
 
 ws.onopen = function (event) {
    console.log(event)
-}
+   ws.send(JSON.stringify({
+       type: "init",
+       tankId: +new Date()
+   }))
+ }
 
 ws.onmessage = function (event) {
    console.log(event)
