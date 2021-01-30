671206a308879a1c1bac72a8963aca2f8305ae1b
Oleh Kutsenko <inthedark1225@gmail.com>
2021-01-30 02:25:27 +0200

    step-12 loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './ws/ws'
+import {useState, useEffect} from 'react'
+import {ws} from './ws/ws'
 import {Grid} from './Grid/Grid';
 import {Tank} from './Tank/Tank'
 
 export function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    ws.addEventListener("open", () => {
+      setIsLoading(false)
+    })
+  }, [])
+
+  if (isLoading) {
+    return <div>Загрузка...</div>
+  }
+
   return (
     <div>
       <Grid></Grid>
